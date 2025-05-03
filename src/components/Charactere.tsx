a1327d338af3016c0612ff9686d648187040545a
Thomas Barrial <thomas@barrial.fr>
2025-05-03 18:49:33 -0400

    Setup phisics and base charatere Gameplay

--- src/components/Charactere.tsx
+++ src/components/Charactere.tsx
@@ -1,4 +1,4 @@
-import { useGLTF, useKeyboardControls } from "@react-three/drei";
+import { useKeyboardControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import {
   CapsuleCollider,
@@ -8,31 +8,30 @@ import {
 } from "@react-three/rapier";
 import React, { useEffect, useRef } from "react";
 import * as THREE from "three";
+import { CharactereModel } from "./models/CharactereModel";
 
 function Charactere() {
   const rigidBodyRef = useRef<RapierRigidBody>(null);
   const cameraFollowRef = useRef<THREE.Object3D>(null);
-  const modelRef = useRef<THREE.Object3D>(null);
   const jumpCooldown = useRef(0);
-  const { scene } = useGLTF("/models/charactere.gltf");
   const [subscribeKeys, get] = useKeyboardControls();
-  const speed = 5;
+  const speed = 3;
   const direction = new THREE.Vector3();
-
+  const isGrounded = useRef(false);
   const { rapier, world } = useRapier();
+  const rayHelper = useRef<THREE.ArrowHelper>(null);
 
   const jump = React.useCallback(() => {
     if (!rigidBodyRef.current) return;
-    if (jumpCooldown.current > 0) return; // ← AJOUT ESSENTIEL
+    if (jumpCooldown.current > 0) return;
 
-    const origin = rigidBodyRef.current.translation();
-    origin.y -= 0.6;
-    const direction = { x: 0, y: -1, z: 0 };
-    const ray = new rapier.Ray(origin, direction);
-    const hit = world.castRay(ray, 1, true);
+    const base = rigidBodyRef.current.translation();
+    const origin = { x: base.x, y: base.y - 0.6, z: base.z };
+    const ray = new rapier.Ray(origin, { x: 0, y: -1, z: 0 });
+    const hit = world.castRayAndGetNormal(ray, 0.5, true);
 
-    if (hit && hit.timeOfImpact < 0.05) {
-      rigidBodyRef.current.applyImpulse({ x: 0, y: 3.5, z: 0 }, true);
+    if (hit && hit.timeOfImpact < 0.1) {
+      rigidBodyRef.current.applyImpulse({ x: 0, y: 4, z: 0 }, true);
       jumpCooldown.current = 1;
     }
   }, [rapier, world]);
@@ -50,21 +49,17 @@ function Charactere() {
     };
   }, [subscribeKeys, jump]);
 
-  function enableShadowRecursively(obj: THREE.Object3D) {
-    obj.traverse((child) => {
-      if ((child as THREE.Mesh).isMesh) {
-        child.castShadow = true;
-      }
-    });
-  }
-
-  useEffect(() => {
-    enableShadowRecursively(scene);
-  }, [scene]);
-
   useFrame((state, delta) => {
     const body = rigidBodyRef.current;
     if (!body) return;
+
+    if (rayHelper.current && rigidBodyRef.current) {
+      const origin = rigidBodyRef.current.translation();
+      const rayOrigin = new THREE.Vector3(origin.x, origin.y - 0.5, origin.z);
+
+      rayHelper.current.position.copy(rayOrigin);
+      rayHelper.current.setDirection(new THREE.Vector3(0, -1, 0));
+    }
 
     const { forward, backward, leftward, rightward } = get();
 
@@ -84,26 +79,6 @@ function Charactere() {
     if (jumpCooldown.current > 0) {
       jumpCooldown.current -= delta;
     }
-    // On applique la rotation
-    if (modelRef.current && direction.lengthSq() > 0.0001) {
-      const targetRotation = new THREE.Quaternion();
-      const lookAtDir = direction.clone().normalize();
-
-      // Calcule la direction de regard normale
-      const dummy = new THREE.Object3D();
-      dummy.lookAt(lookAtDir.x, 0, lookAtDir.z);
-
-      // Appliquer une rotation de +90° autour de Y à la direction
-      const rotationOffset = new THREE.Quaternion().setFromAxisAngle(
-        new THREE.Vector3(0, -1, 0),
-        Math.PI / 2,
-      );
-
-      // Combine avec l’offset de 90°
-      targetRotation.multiplyQuaternions(rotationOffset, dummy.quaternion);
-
-      modelRef.current.quaternion.slerp(targetRotation, 10 * delta);
-    }
 
     // CAMERA
     if (!cameraFollowRef.current) return;
@@ -113,7 +88,7 @@ function Charactere() {
     // Smooth follow
     cameraFollowRef.current.position.lerp(bodyPos, 8 * delta);
 
-    const idealOffset = new THREE.Vector3(5, 10, 5); // (inversé selon besoin)
+    const idealOffset = new THREE.Vector3(8, 15, 8); // (inversé selon besoin)
     const idealLookAt = new THREE.Vector3(0, 1, 0);
 
     const cameraPos = cameraFollowRef.current.position.clone().add(idealOffset);
@@ -121,15 +96,13 @@ function Charactere() {
 
     state.camera.position.lerp(cameraPos, 5 * delta);
     state.camera.lookAt(target);
-
-    // }
   });
 
   return (
     <RigidBody
       ref={rigidBodyRef}
       colliders={false}
-      mass={30}
+      mass={1}
       position={[0, 1, 0]}
       enabledRotations={[false, false, false]} // empêche de tomber ou tourner
       friction={1}
@@ -137,13 +110,19 @@ function Charactere() {
     >
       <object3D ref={cameraFollowRef} />
       <CapsuleCollider args={[0.4, 0.4]} />
-      <primitive
-        ref={modelRef}
-        object={scene}
-        scale={0.2}
-        position={[0, 0, 0]}
-        rotation={[0, Math.PI / 2, 0]}
-      />
+      {/* <primitive
+        ref={rayHelper}
+        object={
+          new THREE.ArrowHelper(
+            new THREE.Vector3(0, -1, 0), // direction
+            new THREE.Vector3(0, 0, 0), // origin
+            1, // length
+            0xff0000, // color
+          )
+        }
+      /> */}
+
+      <CharactereModel isGrounded={isGrounded.current} />
     </RigidBody>
   );
 }
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -2,7 +2,7 @@
 import { Canvas } from "@react-three/fiber";
 import { Physics } from "@react-three/rapier";
 import { Suspense } from "react";
-import { KeyboardControls } from "@react-three/drei";
+import { KeyboardControls, OrbitControls } from "@react-three/drei";
 import Scene from "./Scene";
 import Lights from "./lights/Lights";
 
@@ -20,9 +20,10 @@ export default function Experience() {
       <Canvas shadows camera={{ position: [10, 10, 10], fov: 50 }}>
         <color attach="background" args={["#d6e2e9"]} />
 
+        <OrbitControls />
         <Lights />
         <Suspense fallback={null}>
-          <Physics gravity={[0, -12, 0]} debug>
+          <Physics gravity={[0, -12, 0]}>
             <Scene />
           </Physics>
         </Suspense>
